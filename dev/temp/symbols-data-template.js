9b3767b5d34d45a0518ddda4e8cae2faf03ff27f
Web-Nic <1000.alexnik@gmail.com>
2024-10-01 19:45:17 +0300

    start

---
+++ dev/temp/symbols-data-template.js
@@ -0,0 +1,108 @@
+__iconsData: {
+    
+        'arrow-down': {
+            width: '21.78px',
+            height: '11.08px'
+        },
+    
+        'click': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'clip': {
+            width: '22px',
+            height: '24px'
+        },
+    
+        'ellipse': {
+            width: '128.03px',
+            height: '128.03px'
+        },
+    
+        'external-link-2': {
+            width: '16px',
+            height: '16px'
+        },
+    
+        'external-link': {
+            width: '16px',
+            height: '16px'
+        },
+    
+        'ic-stages-1': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'ic-stages-10': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'ic-stages-11': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'ic-stages-12': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'ic-stages-14': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'ic-stages-2': {
+            width: '50px',
+            height: '60px'
+        },
+    
+        'ic-stages-3': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'ic-stages-4': {
+            width: '60px',
+            height: '40px'
+        },
+    
+        'ic-stages-5': {
+            width: '60px',
+            height: '56px'
+        },
+    
+        'ic-stages-6': {
+            width: '54px',
+            height: '60px'
+        },
+    
+        'ic-stages-7': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'ic-stages-8': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'ic-stages-9': {
+            width: '60px',
+            height: '60px'
+        },
+    
+        'round-info-button': {
+            width: '16px',
+            height: '16px'
+        },
+    
+        'success': {
+            width: '40px',
+            height: '38.25px'
+        },
+    
+}
---
+++ markup/components/head/data/data.js
@@ -0,0 +1,14 @@
+head: {
+    defaults: {
+        title: 'Index',
+        indexPage: true
+    },
+    team: {
+        title: 'Team',
+        indexPage: false
+    },
+    home: {
+        title: 'Home',
+        indexPage: false
+    }
+}
